89b9cf9178b4f01ef40842a37242dafe87e1b0ab
shreyans13 <shreyans1313@gmail.com>
2024-04-28 16:23:05 +0530

    Simple Factory

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import StrategyPattern from "./src/StrategyPattern"
 import ObserverPattern from "./src/ObserverPattern"
 import DecoratorPattern from "./src/DecoratorPattern"
+import SimpleFactory from "./src/FactoryPattern/SimpleFactory"
 
 console.log("Init Learning")
 StrategyPattern();
 ObserverPattern();
 DecoratorPattern();
+SimpleFactory();
---
+++ src/FactoryPattern/SimpleFactory.ts
@@ -0,0 +1,64 @@
+abstract class Pizza {
+  abstract pizzaName(): string;
+  prepare(): void {
+    console.log(`Preparing ${this.pizzaName()}`);
+  }
+
+  bake(): void {
+    console.log(`Baking ${this.pizzaName()}`);
+  }
+
+  cut(): void {
+    console.log(`Cut ${this.pizzaName()}`);
+  }
+
+  box(): void {
+    console.log(`Box ${this.pizzaName()}`);
+  }
+
+}
+
+class CheesePizza extends Pizza {
+  pizzaName = (): string => "CheesePizza";
+}
+
+class PepperoniPizza extends Pizza {
+  pizzaName = (): string => "PepperoniPizza";
+}
+
+
+
+class SimplePizzaFactory {
+  createPizza(type: string): Pizza {
+    return (type.toLowerCase() === "cheese") ?
+      new CheesePizza() : new PepperoniPizza()
+  }
+}
+
+class PizzaStore {
+  factory: SimplePizzaFactory;
+
+  constructor(pizzaFactory: SimplePizzaFactory) {
+    this.factory = pizzaFactory;
+  }
+
+  orderPizza(type: string) {
+    const pizza: Pizza = this.factory.createPizza(type);
+
+    pizza.prepare();
+    pizza.bake();
+    pizza.cut();
+    pizza.box();
+
+    return pizza;
+  }
+
+}
+
+const run = (): void => {
+  const pizzaStore = new PizzaStore(new SimplePizzaFactory());
+  pizzaStore.orderPizza("cheese");
+};
+
+export default run;
+
